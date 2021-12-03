4c3b45139ec8ac362d0faa42d711e3187a2ca09b
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-12-03 12:04:04 -0600

    uploadBudget function, save Record, event listener for comming back online

--- public/js/idb.js
+++ public/js/idb.js
@@ -18,6 +18,48 @@ request.onerror = function (event) {
     console.log(event.target.error)
 };
 
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    budgetObjectStore.add(record);
+};
+
+function uploadBudgetEvent() {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    const getAll = budgetObjectStore.getAll();
+getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_budget'], 'readwrite');
+          const budgetObjectStore = transaction.objectStore('new_budget');
+          budgetObjectStore.clear();
+
+          alert('All saved transactions has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+    
+  }
+
+  // listen for app coming back
+  window.addEventListener('online', uploadBudgetEvent);
 
 
 
